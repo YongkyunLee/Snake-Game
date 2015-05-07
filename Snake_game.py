5762d29ef31dd4bb9ac471a5dfb89911fdae6323
YongkyunLee <ykhl1itj@naver.com>
2015-05-07 11:13:38 +0900

    Integration of snake_only to snake_game

--- Snake_game.py
+++ Snake_game.py
@@ -3,88 +3,112 @@ import random
 import time
 
 class Snake:
-    def __init__(self, canvas, fruit):
+    def __init__(self, canvas):
         self.canvas = canvas
-        self.fruit = fruit
-        self.snake_id = canvas.create_rectangle(0, 0, 10, 10, fill="blue")
-        self.canvas.move(self.snake_id, 10, 10)
-        self.body = [[2,2]]
-        self.body_length = len(self.body)
-        self.canvas.bind_all('<KeyPress-Left>', self.move_left)
-        self.canvas.bind_all('<KeyPress-Right>', self.move_right)
-        self.canvas.bind_all('<KeyPress-Down>', self.move_down)
-        self.canvas.bind_all('<KeyPress-Up>', self.move_up)
-        self.press_key = 1
-        self.begin = 0
-        self.suicide = False
-    def move_right(self, evt):
-        co_x1 = self.body[0][0]
-        co_y1 = self.body[0][1]
-        self.body.insert(0, [co_x1+1, co_y1])
-        self.body_length = len(self.body)
-        del self.body[self.body_length-1]
-        self.press_key = 0
-    def move_left(self, evt):
-        co_x1 = self.body[0][0]
-        co_y1 = self.body[0][1]
-        self.body.insert(0, [co_x1-1, co_y1])
-        self.body_length = len(self.body)
-        del self.body[self.body_length-1]
-        self.press_key = 0
-    def move_up(self, evt):
-        co_x1 = self.body[0][0]
-        co_y1 = self.body[0][1]
-        self.body.insert(0, [co_x1, co_y1-1])
-        self.body_length = len(self.body)
-        del self.body[self.body_length-1]
-        self.press_key = 0
-    def move_down(self, evt):
-        co_x1 = self.body[0][0]
-        co_y1 = self.body[0][1]
-        self.body.insert(0, [co_x1, co_y1+1])
-        self.body_length = len(self.body)
-        del self.body[self.body_length-1]
-        self.press_key = 0
-    def eat(self):
-        if self.body[0] == self.fruit.pos:
-            return True
-        else:
-            return False
-    def out_of_bound(self):
-        if self.body[0][0] > 50 or self.body[0][0] < 0 or self.body[0][1] > 50 or self.body[0][1] < 0:
-            return True
-        else:
-            return False
-    def draw(self):
-        co_x1 = self.body[0][0]
-        co_y1 = self.body[0][1]
-        if self.press_key == 1 and self.begin != 0:
-            if self.body[0][0] - self.body[1][0] == 1:
-                self.body.insert(0, [co_x1+1, co_y1])
-            elif self.body[0][0] - self.body[1][0] == -1:
-                self.body.insert(0, [co_x1-1, co_y1])
-            elif self.body[0][1] - self.body[1][1] == -1:
-                self.body.insert(0, [co_x1, co_y1-1])
-            elif self.body[0][1] - self.body[1][1] == 1:
-                self.body.insert(0, [co_x1, co_y1+1])
-        if self.eat() == False:
-            self.body_length = len(self.body)
-            del self.body[self.body_length-1]
-        self.body_length = len(self.body)
-        self.press_key = 1
-        self.begin = 1
-        for x in range(0, self.body_length):
-            for y in range(x+1, self.body_length):
-                if self.body[x] == self.body[y]:
-                    self.suicide = True
+        self.body = [[6, 2], [5, 2], [4, 2], [3, 2], [2, 2]]
+        for x in range(0, len(self.body)):
             co_x = self.body[x][0]
             co_y = self.body[x][1]
             can_x1 = (co_x - 1) * 10
             can_y1 = (co_y - 1) * 10
             can_x2 = co_x * 10
             can_y2 = co_y * 10
-            self.press_key = 1
-            draw_list[x] = self.canvas.create_rectangle(can_x1, can_y1, can_x2, can_y2, fill="blue", outline="white")        
+            self.canvas.create_rectangle(can_x1, can_y1, can_x2, can_y2, fill="blue", outline="white")
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Up>', self.turn_up)
+        self.canvas.bind_all('<KeyPress-Down>', self.turn_down)
+        self.press_key = 1
+        self.dir = 0
+        self.begin_move = 0
+        self.started = False
+        self.det = False
+        self.move_count = 0
+    def turn_right(self, evt):
+        self.dir = 1
+        self.move_count += 1
+    def turn_left(self, evt):
+        self.dir = 2
+        self.move_count += 1
+    def turn_up(self, evt):
+        self.dir = 3
+        self.move_count += 1
+    def turn_down(self, evt):
+        self.dir = 4
+        self.move_count += 1
+    def move(self):
+        self.co_x = self.body[0][0]
+        self.co_y = self.body[0][1]
+        if dir == 1: #right
+            self.body.insert(0, [self.co_x+1, self.co_y])
+            del self.body[len(self.body)-1]
+            self.press_key = 0
+        if dir == 2: #left
+            self.body.insert(0, [self.co_x-1, self.co_y])
+            del self.body[len(self.body)-1]
+            self.press_key = 0
+        if dir == 3: #up
+            self.body.insert(0, [self.co_x, self.co_y-1])
+            del self.body[len(self.body)-1]
+            self.press_key = 0
+        if dir == 4: #down
+            self.body.insert(0, [self.co_x, self.co_y+1])
+            del self.body[len(self.body)-1]
+            self.press_key = 0
+        if self.press_key == 1:
+            if self.dir == 1: #moving right
+                self.body.insert(0, [self.co_x+1, self.co_y])
+                del self.body[len(self.body)-1]
+            if self.dir == 2: #moving left
+                self.body.insert(0, [self.co_x-1, self.co_y])
+                del self.body[len(self.body)-1]
+            if self.dir == 3: #moving up
+                self.body.insert(0, [self.co_x, self.co_y-1])
+                del self.body[len(self.body)-1]
+            if self.dir == 4: #moving down
+                self.body.insert(0, [self.co_x, self.co_y+1])
+                del self.body[len(self.body)-1]
+    def out_of_bounds(self):
+        if self.body[0][0] > 50 or self.body[0][0] < 1 or self.body[0][1] > 50 or self.body[0][1] < 1:
+            return True
+        else:
+            return False
+    def suicide(self):
+        for x in range(0, len(self.body)):
+            for y in range(x+1, len(self.body)):
+                if self.body[x] == self.body[y]:
+                    self.det = True
+                    break
+            if self.det == True:
+                return True
+                break
+        return False        
+    def eat(self):
+        if self.body[0] == self.fruit.pos:
+            return True
+        else:
+            return False    
+    def draw(self):
+        head_x = self.body[0][0]
+        head_y = self.body[0][1]
+        can_x1 = (head_x - 1) * 10
+        can_y1 = (head_y - 1) * 10
+        can_x2 = head_x * 10
+        can_y2 = head_y * 10
+        self.canvas.create_rectangle(can_x1, can_y1, can_x2, can_y2, fill="blue", outline="white")
+        self.press_key = 1
+    def undraw(self):
+        tail_x = self.body[len(self.body)-1][0]
+        tail_y = self.body[len(self.body)-1][1]
+        can_x1 = (tail_x - 1) * 10
+        can_y1 = (tail_y - 1) * 10
+        can_x2 = tail_x * 10
+        can_y2 = tail_y * 10
+        if self.move_count != 0:
+            self.canvas.create_rectangle(can_x1, can_y1, can_x2, can_y2, fill="white", outline="white")
+        if self.move_count == 1:
+            self.canvas.create_rectangle(10, 10, 20, 20, fill="white", outline="white")
+        self.press_key = 1        
         
 class Fruit:
     def __init__(self, canvas):
@@ -112,27 +136,19 @@ fruit = Fruit(canvas)
 snake = Snake(canvas, fruit)
 
 while 1:
-    if snake.suicide == False and snake.out_of_bound() == False:
-        canvas.delete("all")
-        if snake.eat() == True:
-            while True:
-                co_x = random.randint(1, 50)
-                co_y = random.randint(1, 50)
-                for x in range(0, len(snake.body)-1):
-                    if [co_x, co_y] != snake.body[x]:
-                        continue
-                        overlap = True
-                    else:
-                        overlap = False
-                        break
-                if overlap == True:
-                    continue
-                else:
-                    break
-            fruit.draw(co_x, co_y)
-        else:
-            fruit.draw(3, 3)
-    snake.draw()
-    tk.update_idletasks()
-    tk.update()
-    time.sleep(0.1)
+    if snake.out_of_bounds() == False and snake.suicide() == False:
+        print(snake.body)
+        print(snake.move_count)
+        snake.move()
+        snake.draw()
+        tk.update_idletasks()
+        tk.update()
+        time.sleep(0.05)
+        snake.undraw()
+        tk.update_idletasks()
+        tk.update()
+        #canvas.delete("all")
+        #tk.update_idletasks()
+        #tk.update()
+    else:
+        break
